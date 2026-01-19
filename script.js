f3e57c54417f5e75cfecd986c7b66f99a4ac1ad5
mariaroudenko <maria.roudenko@gmail.com>
2026-01-19 21:06:32 +0100

    commit on main

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let folio_xml = tei_xml.concat(extension);
 let page = document.getElementById("page");
 let pageN = page.innerHTML;
 let number = Number(pageN);
-
 // Loading the IIIF manifest
 var mirador = Mirador.viewer({
   "id": "my-mirador",
